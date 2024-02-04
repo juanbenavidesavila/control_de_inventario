9c7cf7791096f677b6a866d7718963fa9041bd04
juanbenavidesavila <130180526+juanbenavidesavila@users.noreply.github.com>
2024-02-04 19:55:03 +0000

    add_p3

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application, application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = application
 
 application = get_wsgi_application()
